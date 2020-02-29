6c82c918cc5f63fd48053b86e5f73b718aaa5503
Gunish Jain <gunish.jain@gmail.com>
2020-02-29 19:02:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quotemvvm/ui/quotes/QuoteActivity.kt
@@ -0,0 +1,13 @@
+package com.example.quotemvvm.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quotemvvm.R
+
+class QuoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quote)
+    }
+}
