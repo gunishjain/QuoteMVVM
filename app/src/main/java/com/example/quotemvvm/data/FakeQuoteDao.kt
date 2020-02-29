63d819be04d1da46735082e0b6298f98c208b4fb
Gunish Jain <gunish.jain@gmail.com>
2020-02-29 19:18:54 +0530

    Made data class for quotes and added DAO

---
+++ app/src/main/java/com/example/quotemvvm/data/FakeQuoteDao.kt
@@ -0,0 +1,28 @@
+package com.example.quotemvvm.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class FakeQuoteDao {
+    // A fake database table
+    private val quoteList = mutableListOf<Quote>()
+
+    private val quotes = MutableLiveData<List<Quote>>()
+
+    init {
+
+        quotes.value = quoteList
+    }
+
+    fun addQuote(quote: Quote) {
+        quoteList.add(quote)
+        // After adding a quote to the "database",
+        // update the value of MutableLiveData
+        // which will notify its active observers
+        quotes.value = quoteList
+    }
+
+    // Casting MutableLiveData to LiveData because its value
+    // shouldn't be changed from other classes
+    fun getQuotes() = quotes as LiveData<List<Quote>>
+}
---
+++ app/src/main/java/com/example/quotemvvm/data/Quote.kt
@@ -0,0 +1,9 @@
+package com.example.quotemvvm.data
+
+data class Quote( val quoteText: String,
+                   val author: String) {
+    override fun toString(): String {
+        return "$quoteText - $author"
+    }
+}
+
